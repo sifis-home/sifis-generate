0f4ff0d31efd294f9b0f9da91e8b74f419bb0d5b
Luca Barbato <lu_zero@gentoo.org>
2021-10-11 10:28:23 +0200

    Initial cli

--- src/bin/sifis-tool.rs
+++ src/bin/sifis-tool.rs
@@ -1,3 +1,28 @@
-fn main() {
+use std::path::PathBuf;
 
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Opts {
+    #[structopt(subcommand)]
+    cmd: Cmd,
+}
+
+#[derive(StructOpt, Debug)]
+enum Cmd {
+    /// Create a new project
+    New {
+        /// Name of a builtin template
+        #[structopt(long, short)]
+        template: String,
+        /// Path to the new project
+        #[structopt(parse(from_os_str))]
+        project_name: PathBuf,
+    },
+}
+
+fn main() {
+    let opts = Opts::from_args();
+
+    println!("{:?}", opts);
 }
