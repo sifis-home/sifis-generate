75208220a6c751e97d16a3f41fe49fe6f54ed345
Luni-4 <luni-4@hotmail.it>
2021-10-13 09:29:05 +0200

    Implement meson template
    The meson template defines a typical meson project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,57 @@
-use std::{collections::HashMap, path::PathBuf};
+mod meson;
 
-use minijinja::Source;
+use std::path::Path;
+use std::str::FromStr;
 
-/// Project template
-///
-/// A collection of templates with their destination paths within the project
-#[derive(Debug)]
-struct ProjectTemplate {
-    /// Contains the output path <-> template name association
-    files: HashMap<PathBuf, String>,
-    /// Storage for the templates
-    templates: Source,
+use anyhow::{bail, Result};
+
+use crate::meson::*;
+
+/// Supported templates
+enum Templates {
+    MesonC,
+    MesonCpp,
+}
+
+impl FromStr for Templates {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "meson-c" => Ok(Self::MesonC),
+            "meson-c++" => Ok(Self::MesonCpp),
+            _ => Err(()),
+        }
+    }
+}
+
+/// Render a template
+trait RenderTemplate {
+    fn render(&self, project_name: &str) -> Result<()>;
+}
+
+/// Creates a new project
+pub fn create_project(template: &str, project_path: &Path) -> Result<()> {
+    let project_name = if let Some(os_name) = project_path.file_name() {
+        if let Some(name) = os_name.to_str() {
+            name
+        } else {
+            bail!("Impossible to convert the project name into a valid Unicode string");
+        }
+    } else {
+        bail!("Impossible to get the project name");
+    };
+
+    let template_type = if let Ok(template_type) = Templates::from_str(template) {
+        template_type
+    } else {
+        bail!("Wrong template name!");
+    };
+
+    let template_builder = match template_type {
+        Templates::MesonC => Meson::c_template(project_path),
+        Templates::MesonCpp => Meson::cpp_template(project_path),
+    };
+
+    template_builder.render(project_name)
 }
