2b04b3d2491f4bb3077ac457da7619ab73779e4a
Luni-4 <luni-4@hotmail.it>
2021-10-21 23:34:16 +0200

    Add docker template

---
+++ templates/meson/run_tests.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+meson setup --buildtype release .build-directory
+meson compile -C .build-directory
+meson setup -Db_coverage=true .build-directory-coverage
+meson test -C .build-directory-coverage
+ninja coverage-xml -C .build-directory-coverage
+meson setup --buildtype release -Db_sanitize=address -Db_lundef=false .build-directory-asan
+meson test -C .build-directory-asan
