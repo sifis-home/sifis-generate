82f6101e2c37018cf5a4a75364a9182c6d6953ad
Luca Barbato <lu_zero@gentoo.org>
2021-11-08 15:49:27 +0100

    Make the context an explicit HashMap

--- src/cargo.rs
+++ src/cargo.rs
@@ -39,10 +39,14 @@ impl BuildTemplate for Cargo {
         &self,
         project_path: &Path,
         project_name: &str,
-    ) -> (HashMap<PathBuf, &'static str>, Vec<PathBuf>, Value) {
+    ) -> (
+        HashMap<PathBuf, &'static str>,
+        Vec<PathBuf>,
+        HashMap<&'static str, Value>,
+    ) {
         let (files, dirs) = Cargo::project_structure(project_path, project_name);
 
-        (files, dirs, ().into())
+        (files, dirs, HashMap::new())
     }
 
     fn get_templates() -> &'static [(&'static str, &'static str)] {
